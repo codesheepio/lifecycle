cf4355e330b29ad57628c87ad9dcb555bf41cf89
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-06-30 01:46:40 +0700

    Add log in component lifecycle

--- src/components/ColoredCounter.js
+++ src/components/ColoredCounter.js
@@ -7,12 +7,33 @@ export default class ColoredCounter extends Component {
     this.state = { color: '#f00' }
     this.changeColor = this.changeColor.bind(this)
   }
+  componentWillMount() {
+    console.log('[ColoredCounter] componentWillMount')
+  }
+  componentDidMount() {
+    console.log('[ColoredCounter] componentDidMount')
+  }
+  componentWillReceiveProps() {
+    console.log('[ColoredCounter] componentWillReceiveProps')
+  }
+  shouldComponentUpdate() {
+    console.log('[ColoredCounter] shouldComponentUpdate')
+    return true
+  }
+  componentWillUpdate() {
+    console.log('[ColoredCounter] componentWillUpdate')
+  }
+  componentDidUpdate() {
+    console.log('[ColoredCounter] componentDidUpdate')
+  }
+
   changeColor(event) {
     this.setState({
       color: event.target.value,
     })
   }
   render() {
+    console.log('[ColoredCounter] render')
     return (
       <div id="counter">
         <select name="color" onChange={this.changeColor}>
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,12 +6,32 @@ export default class Counter extends Component {
     this.state = { counter: 0 }
     this.count = this.count.bind(this)
   }
+  componentWillMount() {
+    console.log('[Counter] componentWillMount')
+  }
+  componentDidMount() {
+    console.log('[Counter] componentDidMount')
+  }
+  componentWillReceiveProps() {
+    console.log('[Counter] componentWillReceiveProps')
+  }
+  shouldComponentUpdate() {
+    console.log('[Counter] shouldComponentUpdate')
+    return true
+  }
+  componentWillUpdate() {
+    console.log('[Counter] componentWillUpdate')
+  }
+  componentDidUpdate() {
+    console.log('[Counter] componentDidUpdate')
+  }
   count() {
     this.setState({
       counter: this.state.counter + 1,
     })
   }
   render() {
+    console.log('[Counter] render')
     return (
       <div id="counter" style={{ color: this.props.color }}>
         <div>
